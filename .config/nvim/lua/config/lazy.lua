392fa8b40c5e75b26ba8f8c28878ac8cb836236d
Łukasz Myśliński <lukasz@mlnative.com>
2024-06-22 11:18:45 +0200

    Various updates

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
   },
