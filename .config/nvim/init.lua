5c733ebcc1e359cfdab03e9e839a7f658f0ccaf4
Lukasz Myslinski <lukasz@mlnative.com>
2025-03-02 09:40:55 +0100

    telescope works

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,6 +27,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 local plugins = {
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }  },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
 local opts = {}
@@ -34,5 +35,9 @@ local opts = {}
 require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
 vim.cmd.colorscheme("catppuccin")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
 
