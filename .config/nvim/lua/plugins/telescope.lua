e795d96d62bf753bb6750c3305a7217095b3c06c
Lukasz Myslinski <lukasz@mlnative.com>
2025-03-02 13:19:24 +0100

    fix hotkeys

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
   end
   }
