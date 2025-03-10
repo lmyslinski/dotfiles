654d3865c44c4d53329ff933fed0fcd94e252c9e
Lukasz Myslinski <lukasz@mlnative.com>
2025-03-10 18:55:29 +0100

    formatting with null-ls

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,19 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      follow_current_file = {
-        enabled = true
-      }
-    })
-    vim.keymap.set('n', '<C-n>', ':Neotree toggle <CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			follow_current_file = {
+				enabled = true,
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle <CR>")
+	end,
 }
-
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build= ":TSUpdate",
-  config = function ()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "javascript", "typescript", "tsx", "html", "markdown", "python"},
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { enable = true },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
-
