85a24a2c4e5362f469cab359aba015fe69ce0bab
lmyslinski <lukasz@mlnative.com>
2025-07-19 15:32:51 +0200

    chore: more aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,3 +2,11 @@ alias vim='nvim'
 alias tf='terraform'
 alias dc='docker compose'
 alias n='nvim'
+
+alias ls='eza -lh --group-directories-first --icons=auto'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+alias ff="fzf --preview 'batcat --style=numbers --color=always {}'"
+alias fd='fdfind'
+alias cd='z'
